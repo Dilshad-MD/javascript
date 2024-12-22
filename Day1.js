77d7f93945eb37c83a1ed041f921452e68f7527c
Dilshad-MD <mddilshad56056@gmail.com>
2024-12-22 21:23:33 +0530

    JS

---
+++ Day1.js
@@ -0,0 +1,66 @@
+
+// variables
+// var age = 10 ;
+// const num = 20 ;
+// let a = 30;
+//  age = 20;
+    // a = 20
+// console.log(a)
+
+// Objects
+var person = {
+    firstName: "Nirmal",
+    lastName: "Kumar",
+    age : 22,
+    Nationality: "Indian"
+}
+// console.log(person.firstName)
+
+// Array method
+
+// var fruit = ["Banana", "Apple", "Pear"];
+// console.log(fruit);
+
+// concat methon
+
+// console.log(fruit.concat(["nirmal"]));
+// console.log(fruit.concat([34]));
+
+// indexpof method
+
+// console.log(fruit.indexOf("Banana"))
+// console.log(fruit.indexOf("apple"))
+// console.log(fruit.indexOf(1))
+
+
+// Jion Method
+// var fruit = ["Banana", "Apple", "Pear"];
+// var contain = ["Papaya ", "Orange", "Grapes"]
+
+// // console.log(fruit.join())
+
+// var com = fruit.concat(contain);
+// var fruit = contain.join()
+// var result = com.join(",")
+// console.log(result);
+
+// Last Index method
+
+// var fruit = ["Banana", "Apple", "Pear"];
+// console.log(fruit.lastIndexOf("Pear"));
+// console.log(fruit.lastIndexOf("Apple"));
+// console.log(fruit.lastIndexOf("Banana"));
+// console.log(fruit.lastIndexOf(1))
+
+// Pop 
+
+
+// var fruit = ["Banana", "Apple", "Pear"];
+//  console.log(fruit.pop())
+//  console.log(fruit)
+
+// push method
+
+var fruit = ["Banana", "Apple", "Pear"];
+console.log(fruit.push("papaya"))
+console.log(fruit)
