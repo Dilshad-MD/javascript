af5940bce821eee9c7ffffeacd76008159a27e37
Dilshad-MD <mddilshad56056@gmail.com>
2024-12-23 21:14:06 +0530

    js practice

---
+++ Day2.js
@@ -0,0 +1,53 @@
+// Revesre method
+
+// let a = ["Nirmal", "Salman", "Manjeet"];
+// console.log(a.reverse());
+
+
+// Shift Method 
+
+// let a = ["Nirmal", "Salman", "Manjeet"];
+// console.log(a.shift());
+// console.log(a)
+
+// Slice method
+// let a = ["Nirmal", "Salman", "Manjeet"];
+// // console.log(a.slice(1 , 3))
+// console.log(a.slice(2))
+// console.log(a)
+
+//Sort Method 
+
+// let a = ["Nirmal", "Salman", "Manjeet", "Gufran", "Niranjan"];
+// console.log(a.sort())
+
+// Splice Method
+// let a = ["Nirmal", "Salman" , "Manjeet" ]
+// console.log(a.splice(1, 2));
+//  a.splice(0, 3, "Gufran", "Pathak", "Dilshad")
+// console.log(a)
+
+// toString Method
+
+// let num = [1, 2, 3];
+//     console.log(num.toString())
+
+
+
+// unshift method
+
+// let a = ["Nirmal", "Gufran", "Salman", "Majeet" ];
+//     a.unshift("Dilshad")
+//     console.log(a)
+
+
+// valueof method 
+
+ 
+    // const obj = {
+    //     value: 100,
+    //     valueOf: function () {
+    //       return this.value;
+    //     },
+    //   };
+    //   console.log(obj.valueOf())
