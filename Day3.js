936b6589233575efbaa513e41be31a6acdbef78d
Dilshad-MD <mddilshad56056@gmail.com>
2024-12-24 21:37:09 +0530

    javascript

---
+++ Day3.js
@@ -0,0 +1,143 @@
+//  Basics Operators
+
+// Addtion Operators
+// let a = 10;
+// let b = 20;
+// let c = a+b;
+// console.log(c)
+
+//Subtraction Operator
+// let a = 34;
+// let b = 23;
+// let c = a-b;
+// console.log(c);
+
+// Multiplication
+
+// let  a = 4;
+// let b = 2;
+// let c= a*b;
+// console.log(c)
+
+// Division Operator
+
+// let a = 34;
+// let b = 2;
+// let c = a/b;
+// console.log(c)
+
+// Modulus Operator
+
+// let a = 79;
+// let b = 5;
+// let c = a%b;
+// console.log(c)
+
+// Increment Operator
+
+// let a = 10;
+// let b = a++;
+// let c = b;
+// console.log(c)
+
+// Decrement Operator
+
+// let a = 10;
+// let b = a--;
+// let c= b;
+// console.log(c)
+
+// Comparison Operators
+
+//equal to
+// let a = 10;
+// let b = "10";
+// console.log(a==b);
+
+// equal value and equal type
+// let a = 10;
+// let b= 20;
+// console.log(a===b);
+
+// let a = 10;
+// let b= "10";
+// console.log(a===b)
+
+// let a = 10;
+// let b= 10;
+// console.log(a===b)
+
+// Not equal
+
+// let a = 10;
+// let b= 20;
+// console.log(a!=b)
+
+// let a = 10;
+// let b= 10;
+// console.log(a!=b)
+
+//Greater than
+
+
+// let a = 10;
+// let b= 20;
+// console.log(b>a)
+
+// let a = 10;
+// let b= 20;
+// console.log(a>b)
+
+// Less than
+
+// let a = 10;
+// let b= 20;
+// console.log(a<b)
+
+// let a = 10;
+// let b= 20;
+// console.log(b< a)
+
+//Greater Than Equal to
+
+// let a = 10;
+// let b= 20;
+// console.log(a>=b)
+
+// let a = 10;
+// let b= 20;
+// console.log(b>=a)
+
+// Less Than Equal to 
+
+// let a = 10;
+// let b= 20;
+// console.log(a<=b)
+
+// let a = 10;
+// let b= 20;
+// console.log(b<=a)
+
+// Logiacl Operators
+
+// Logical &&
+
+// let a = true;
+// let b = false;
+// console.log(a && b);
+// console.log(a && !b)
+
+// Logical ||
+// let a = true;
+// let b = false;
+// console.log(a || b);
+// console.log(b || b)
+
+// Logical !
+
+
+let a = true;
+console.log(!a);
+
+let b = !true;
+console.log(!b)
