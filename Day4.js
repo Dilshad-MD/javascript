e5bd9e5c4cab0cede62b6b12b431fd5acfe98a07
Dilshad-MD <113435059+Dilshad-MD@users.noreply.github.com>
2024-12-26 00:22:05 +0530

    Add files via upload

---
+++ Day4.js
@@ -0,0 +1,20 @@
+// & Bitwise Operator
+
+// let x = 5;
+// let y = 1;
+// console.log(x & y);
+
+// | Bitwise Operator
+
+// let x = 5;
+// let y = 1;
+// console.log(x | y);
+
+// ~ Not bitwise operator
+
+// let x = 5;
+// console.log(~x)
+
+console.log(~10);
+
+
