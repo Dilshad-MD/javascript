15291ca7b4570eed1eb41803eb514dd7e498b51f
Dilshad-MD <mddilshad56056@gmail.com>
2024-12-27 21:56:34 +0530

    javascript

---
+++ day6/day6.js
@@ -0,0 +1,109 @@
+// Function Method
+
+// function showAlert(message){
+//    window.alert(message);
+// }
+// showAlert("welcome to our web ")
+
+
+// function showAlert(message){
+//     window.confirm(message);
+//  }
+//  showAlert("welcome to our web ")
+
+
+// function calculateCartPrice(num1){
+//     return num1;
+
+// }
+// console.log(calculateCartPrice(200, 3000, 4000))
+
+// function calculateCartPrice(...num1){
+//     return num1;
+
+// }
+// console.log(calculateCartPrice(200, 3000, 4000))
+
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1;
+
+// }
+// console.log(calculateCartPrice(200, 3000, 4000, 3500))
+
+
+// const users ={
+//     userName :  "Nirmal",
+//     price : 199,
+// }
+
+// function handleObject(anyObject){
+//     console.log(`UserName  is ${anyObject.userName} and price is ${anyObject.price} `);
+
+// }
+// handleObject(users)
+
+
+
+// const users ={
+//         userName :  "Nirmal",
+//         price : 199,
+//     }
+    
+//     function handleObject(anyObject){
+//         console.log(`UserName  is ${anyObject.userName} and price is ${anyObject.price} `);
+    
+//     }
+//     handleObject({
+//         userName: "salman",
+//         price: 299,
+//     })
+
+
+// const arr = [200, 300, 400, 500];
+
+// function returnSecondValue(getArray){
+//     return getArray[1];
+// }
+// console.log(returnSecondValue(arr));
+
+
+// scope
+
+// var a = 10;
+// const b = 20;
+// let c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// if(true){
+//  var a = 10;
+//  const b = 20;
+//  let c = 30;
+// }
+//  console.log(a);
+// console.log(b);
+// console.log(c); 
+
+// var a = 30;
+
+// if(true){
+//     var a = 10;
+//     const b = 20;
+//     let c = 30;
+//    }
+// console.log(a);
+// console.log(b);
+// console.log(c); 
+
+let c = 40;
+if(true){
+    var a = 10;
+    const b = 20;
+    let c = 30;
+    console.log(c)
+   }
+console.log(c);
